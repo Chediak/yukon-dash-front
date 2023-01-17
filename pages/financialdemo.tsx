6a9ca1236c39c5f68c7d6839b345a12592d8d0c6
unknown <shammcity00@gmail.com>
2023-01-17 12:31:01 -0300

    cardash finance

--- pages/financialdemo.tsx
+++ pages/financialdemo.tsx
@@ -38,34 +38,41 @@ import SearchBar from "../components/SearchBar";
 import SimpleSidebar from "../components/Sidebar";
 
 const links = [
-    { title: "Início", link: "/financialdemo" },
-    { title: "Demonstrações Financeiras", link: "/financialdemo" },
-  ];
+  { title: "Início", link: "/financialdemo" },
+  { title: "Demonstrações Financeiras", link: "/financialdemo" },
+];
 
 export default function financialdemo() {
   return (
     <SimpleSidebar>
       <Layout>
-      <BreadCrumb items={links} />
-        <StatGroup>
-          <Stat>
-            <StatLabel>Sent</StatLabel>
-            <StatNumber>345,670</StatNumber>
-            <StatHelpText>
-              <StatArrow type="increase" />
-              23.36%
-            </StatHelpText>
-          </Stat>
+        <BreadCrumb items={links} />
 
-          <Stat>
-            <StatLabel>Clicked</StatLabel>
-            <StatNumber>45</StatNumber>
-            <StatHelpText>
-              <StatArrow type="decrease" />
-              9.05%
-            </StatHelpText>
-          </Stat>
-        </StatGroup>
+        <Box>
+          <CardDash>
+            <Stack direction={"row"}>
+              <StatGroup>
+                <Stat mt={"25px"}>
+                  <StatLabel>Saldo Total</StatLabel>
+                  <StatNumber>35.670,00</StatNumber>
+                  <StatHelpText>
+                    <StatArrow type="increase" />
+                    23.36%
+                  </StatHelpText>
+                </Stat>
+
+                <Stat mt={"25px"}>
+                  <StatLabel>Dívida</StatLabel>
+                  <StatNumber>5.090,00</StatNumber>
+                  <StatHelpText>
+                    <StatArrow type="decrease" />
+                    9.05%
+                  </StatHelpText>
+                </Stat>
+              </StatGroup>
+            </Stack>
+          </CardDash>
+        </Box>
       </Layout>
     </SimpleSidebar>
   );
