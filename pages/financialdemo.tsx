8f8d0cc4cdafacb7b681afe23a509e18e23e39db
unknown <shammcity00@gmail.com>
2023-01-17 14:04:57 -0300

    apexcharts

--- pages/financialdemo.tsx
+++ pages/financialdemo.tsx
@@ -5,6 +5,9 @@ import {
   StatHelpText,
   StatArrow,
   StatGroup,
+  Flex,
+  SimpleGrid,
+  theme,
 } from "@chakra-ui/react";
 
 import {
@@ -29,6 +32,8 @@ import {
   Tooltip,
   Tr,
 } from "@chakra-ui/react";
+
+import dynamic from "next/dynamic";
 import { motion } from "framer-motion";
 import ReactPaginate from "react-paginate";
 import BreadCrumb from "../components/BreadCrumb";
@@ -37,39 +42,93 @@ import Layout from "../components/layouts/Layout";
 import SearchBar from "../components/SearchBar";
 import SimpleSidebar from "../components/Sidebar";
 
+const Chart = dynamic(() => import("react-apexcharts"), {
+  ssr: false,
+});
+
 const links = [
   { title: "Início", link: "/financialdemo" },
   { title: "Demonstrações Financeiras", link: "/financialdemo" },
 ];
 
 export default function financialdemo() {
+  const options = {
+    chart: {
+      toolbar: {
+        show: false,
+      },
+      zoom: {
+        enabled: false,
+      },
+      foreColor: theme.colors.gray[500],
+    },
+    grid: {
+      show: false,
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    tootip: {
+      enabled: false,
+    },
+    xasis: {
+      type: "datetime",
+      axisBorder: {
+        color: theme.colors.gray[100],
+      },
+      axisTicks: {
+        color: theme.colors.gray[100],
+      },
+      categories: [
+        "2022-05-25T11:15:56.000Z",
+        "2022-05-26T11:15:56.000Z",
+        "2022-05-27T22:25:56.000Z",
+        "2022-05-28T13:04:56.000Z",
+        "2022-05-29T11:15:56.000Z",
+        "2022-05-30T01:15:56.000Z",
+        "2022-05-31T11:15:56.000Z",
+      ],
+    },
+    fill: {
+      opacity: 0.3,
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        opacityFrom: 0.7,
+        opacityTo: 0.3,
+      },
+    },
+  };
+
+  const series = [{ name: "series1", data: [101, 120, 10, 208, 154, 204] }];
+
   return (
     <SimpleSidebar>
       <Layout>
         <BreadCrumb items={links} />
-
         <Box>
           <CardDash>
             <Stack direction={"row"}>
-              <StatGroup>
-                <Stat mt={"25px"}>
-                  <StatLabel>Saldo Total</StatLabel>
-                  <StatNumber>35.670,00</StatNumber>
-                  <StatHelpText>
-                    <StatArrow type="increase" />
-                    23.36%
-                  </StatHelpText>
-                </Stat>
-
-                <Stat mt={"25px"}>
-                  <StatLabel>Dívida</StatLabel>
-                  <StatNumber>5.090,00</StatNumber>
-                  <StatHelpText>
-                    <StatArrow type="decrease" />
-                    9.05%
-                  </StatHelpText>
-                </Stat>
-              </StatGroup>
+              <SimpleGrid columns={1}>
+                <Box
+                  p={["6", "8"]}
+                  bg="transparent"
+                  borderRadius={8}
+                  pb="4"
+                  alignItems={"flex-end"}
+                >
+                  <Text fontSize="lg" mb="4">
+                    Gastos Semanais
+                  </Text>
+                  <Chart
+                    type="area"
+                    width="200%"
+                    height="160"
+                    options={options}
+                    series={series}
+                  />
+                </Box>
+              </SimpleGrid>
             </Stack>
           </CardDash>
         </Box>
